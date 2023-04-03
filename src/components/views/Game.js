378254a45dd11f7b9b5560ec68c8b1842164b303
Nilsils <nils.bohnenblust@gmail.com>
2023-04-03 12:50:36 +0200

    Revert "Merge branch '38ff' of https://github.com/sopra-fs23-group-29/sopra-fs23-group-29-client into Client-websocket"
    This reverts commit a64a3747d34e3509a434f1a069f65ad405e55b83, reversing
    changes made to e7ab5b4855d1c4c02b64a4ca592afdba1dc3ff04.

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -1,30 +1,26 @@
-import { useEffect, useState } from "react";
-import { api, handleError } from "helpers/api";
-import { Spinner } from "components/ui/Spinner";
-import { Button } from "components/ui/Button";
-import { useHistory } from "react-router-dom";
+import {useEffect, useState} from 'react';
+import {api, handleError} from 'helpers/api';
+import {Spinner} from 'components/ui/Spinner';
+import {Button} from 'components/ui/Button';
+import {useHistory} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import "styles/views/Game.scss";
 
-const UserList = (props) => {
-  const Player = ({ user }) => (
+
+const Game = props => {
+
+  const Player = ({user}) => (
     <div className="player container">
-      <div className="player username">
-        {
-          <Button width="100%" onClick={() => goToProfile(user.id)}>
-            {user.username}
-          </Button>
-        }
-      </div>
+      <div className="player username">{<Button width = "100%" onClick={() => goToProfile(user.id)}>{user.username}</Button>}</div>
       <div className="player id">ID: {user.id}</div>
       <div className="player id">{user.status}</div>
     </div>
   );
 
   Player.propTypes = {
-    user: PropTypes.object,
-  };
+    user: PropTypes.object
+  };  
 
   // use react-router-dom's hook to access the history
   const history = useHistory();
@@ -39,31 +35,31 @@ const UserList = (props) => {
   const [loggedinUserName, setLoggedinUserName] = useState(null);
 
   const doLogout = async () => {
+
     // set status to offline
     try {
+      
       // set status to offline
       await api.put(
-        "/users/logout",
+        '/users/logout',
         {},
-        {
-          headers: {
-            Authorization: JSON.parse(localStorage.getItem("token")).token,
-          },
-        }
+        {headers:{"Authorization": JSON.parse(localStorage.getItem('token')).token}}
       );
+
     } catch (error) {
       alert(`Something went wrong during the login: \n${handleError(error)}`);
     }
 
     // remove the token
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
+    
+    history.push('/login');
 
-    history.push("/login");
   };
 
   const goToProfile = (id) => {
     history.push(`/profile/${id}`);
-  };
+  }
 
   // the effect hook can be used to react to change in your component.
   // in this case, the effect hook is only run once, the first time the component is mounted
@@ -73,59 +69,57 @@ const UserList = (props) => {
     // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
     async function fetchData() {
       try {
-        const response = await api.get("/users", {
-          headers: {
-            Authorization: JSON.parse(localStorage.getItem("token")).token,
-          },
-        });
+        const response = await api.get(
+          '/users',
+          {headers:{"Authorization": JSON.parse(localStorage.getItem('token')).token}}
+        );
 
         // delays continuous execution of an async operation for 1 second.
         // This is just a fake async call, so that the spinner can be displayed
         // feel free to remove it :)
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        await new Promise(resolve => setTimeout(resolve, 1000));
 
         // Get the returned users and update the state.
         setUsers(response.data);
-
+        
         // This is just some data for you to see what is available.
         // Feel free to remove it.
-        console.log("request to:", response.request.responseURL);
-        console.log("status code:", response.status);
-        console.log("status text:", response.statusText);
-        console.log("requested data:", response.data);
+        console.log('request to:', response.request.responseURL);
+        console.log('status code:', response.status);
+        console.log('status text:', response.statusText);
+        console.log('requested data:', response.data);
 
         // See here to get more data.
         console.log(response);
       } catch (error) {
-        console.error(
-          `Something went wrong while fetching the users: \n${handleError(
-            error
-          )}`
-        );
+        console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
         console.error("Details:", error);
-        alert(
-          "Something went wrong while fetching the users! See the console for details."
-        );
+        alert("Something went wrong while fetching the users! See the console for details.");
       }
     }
 
     fetchData();
   }, []);
 
-  let content = <Spinner />;
+  let content = <Spinner/>;
 
   if (users) {
+
     content = (
       <div className="game">
         <ul className="game user-list">
-          {users.map((user) => (
-            <Player user={user} key={user.id} />
+          {users.map(user => (
+            <Player user={user} key={user.id}/>
           ))}
         </ul>
-
-        <Button width="100%" onClick={() => doLogout()}>
+        
+        <Button
+          width="100%"
+          onClick={() => doLogout()}
+        >
           Logout
         </Button>
+
       </div>
     );
   }
@@ -133,10 +127,12 @@ const UserList = (props) => {
   return (
     <BaseContainer className="game container">
       <h2>Happy Coding!</h2>
-      <p className="game paragraph">Get all users from secure endpoint:</p>
+      <p className="game paragraph">
+        Get all users from secure endpoint:
+      </p>
       {content}
     </BaseContainer>
   );
-};
+}
 
-export default UserList;
+export default Game;
