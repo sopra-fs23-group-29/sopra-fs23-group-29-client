1f077200937c312f5f99d6247d83968c2a6f596d
Thalia Lynn Fox <thalialynn.fox@uzh.ch>
2023-04-12 16:52:01 +0200

    lobby client works, needs call to server

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -22,14 +22,25 @@ const NameFormField = (props) => {
     </div>
   );
 };
+NameFormField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 const Lobby = (props) => {
   const history = useHistory();
   const [gameName, setGameName] = useState(null);
   const [hasName, setHasName] = useState(false);
 
-  const startGame = () => {
+  const createLobby = () => {
+    // const name = gameName;
+    // console.log(name);
     setHasName(true);
+  };
+
+  const startGame = () => {
+    console.log("Game has been started");
   };
 
   const backToLobbyOverview = () => {
@@ -42,13 +53,20 @@ const Lobby = (props) => {
         <div>true</div>
       ) : (
         <ul>
-          <NameFormField />
-          <Button disabled={!gameName} onClick={() => startGame()}>
+          <NameFormField value={gameName} onChange={(un) => setGameName(un)} />
+          <Button disabled={!gameName} onClick={() => createLobby()}>
             Create PvP Lobby
           </Button>
         </ul>
       )}
-      <Button onClick={() => backToLobbyOverview()}>Cancel</Button>
+      {hasName ? (
+        <ul>
+          <Button onClick={() => backToLobbyOverview()}>Exit Lobby</Button>
+          <Button onClick={() => startGame()}>Start Game</Button>
+        </ul>
+      ) : (
+        <Button onClick={() => backToLobbyOverview()}>Cancel</Button>
+      )}
     </BaseContainer>
   );
 };
