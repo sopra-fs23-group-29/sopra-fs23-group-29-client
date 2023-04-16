d03676b613e8a0c882715b36aa2cb7383e22fb02
Thalia Lynn Fox <thalialynn.fox@uzh.ch>
2023-04-17 00:57:48 +0200

    Update SoloGameSettings.js
    sologame display logic for entering specifics (duration/# of tiles)

--- src/components/views/SoloGameSettings.js
+++ src/components/views/SoloGameSettings.js
@@ -9,10 +9,6 @@ import PropTypes from "prop-types";
 
 import Stomper from "../../helpers/Stomp";
 
-const Checkbox = ({ label, value, onChange }) => {
-  return <label></label>;
-};
-
 const SoloGameSettings = (props) => {
   const history = useHistory();
   const [gameMode, setGameMode] = useState(null);
@@ -20,21 +16,30 @@ const SoloGameSettings = (props) => {
   const [isHowFar, setIsHowFar] = useState(false);
   const [isHowFast, setIsHowFast] = useState(false);
 
+  /* executed whenever the How Far option is chosen */
   const howFarChosen = () => {
     setIsHowFar(!isHowFar);
     setIsHowFast(false);
   };
 
+  /* executed whenever the How Fast option is chosen */
   const howFastChosen = () => {
     setIsHowFast(!isHowFast);
     setIsHowFar(false);
   };
 
+  /* useEffect needs to be used here to immediately update ishowfast and ishowfar when a checkbox is clicked*/
   useEffect(() => {
     console.log("far: " + isHowFar);
     console.log("fast: " + isHowFast);
-  }, [isHowFar, isHowFast]);
+    /* Change hasGameMode (for startGame() button display) iff either ishowfar or ishowfast is true*/
+    (isHowFar & !isHowFast) | (!isHowFar & isHowFast)
+      ? setHasGameMode(true)
+      : setHasGameMode(false);
+    console.log("has game mode: " + hasGameMode);
+  }, [isHowFar, isHowFast, hasGameMode]);
 
+  // TODO + figure out how to send specific settings for duration or tiles on game board to the server
   const startGame = async () => {};
 
   const backToLobbyOverview = () => {
@@ -60,7 +65,12 @@ const SoloGameSettings = (props) => {
         ></input>
         See how fast you can go
       </label>
-
+      {isHowFar ? <div>Select how long you want to play for:</div> : <div />}
+      {isHowFast ? (
+        <div>Select the number of tiles on the game board:</div>
+      ) : (
+        <div />
+      )}
       <Button onClick={() => startGame()} disabled={!hasGameMode}>
         Start Game
       </Button>
