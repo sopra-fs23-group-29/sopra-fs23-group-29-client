39ccdc1284be86a3d3dc481367564cba460078b9
moonaaaw <116147381+moonaaaw@users.noreply.github.com>
2023-04-16 20:59:40 +0200

    added basic view of "Countries popping up"

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -13,17 +13,72 @@ const Game = props => {
     const board = new Board(10);
     const history = useHistory();
 
+    // Variable to change right now
+
+    let change = "Countries popping up"
+
+    // Round counter
     let roundNumber = 1
+    const roundTimeout = setTimeout(content, 1000);
     let content = (
         <BaseContainer className="round container">
             Round {roundNumber}
         </BaseContainer>);
 
-    if (0) {
+    // End Turn
+    const endTurn = () => {
+        // dummy
+        history.push("/users");
+    };
+
+    // Countries popping up
+    let category = "Population"
+    let playerNumber = 4
+
+    if (change ="Countries popping up") {
 
         content = (
             <BaseContainer className="middle container">
-                <h2>Happy Coding!</h2>
+                <h2>Category: {category}</h2>
+                <p>When it's your turn, click on a country and
+                    a number to rank the country in relation to
+                    the other countries. Each country can only be chosen once.</p>
+                <div className="middle flag-rows">
+                    <div className="middle flag-container">Switzerland</div>
+                    <div className="middle flag-container">Switzerland</div>
+                    <div className="middle flag-container">Switzerland</div>
+                </div>
+                <div className="middle flag-rows">
+                    <div className="middle flag-container">Switzerland</div>
+                    <div className="middle flag-container">Switzerland</div>
+                    <div className="middle flag-container">Switzerland</div>
+                </div>
+                <div className="middle bottom-row">
+                    <div>[ COUNTDOWN ]</div>
+                    <div className="middle number-row">
+                        <Button className="rank">
+                            1
+                        </Button>
+                        <Button className="rank">
+                            2
+                        </Button>
+                        <Button className="rank">
+                            3
+                        </Button>
+                        <Button className="rank">
+                            4
+                        </Button>
+                        <Button className="rank">
+                            5
+                        </Button>
+                        <Button className="rank">
+                            6
+                        </Button>
+                    </div>
+                    <Button onClick={() => endTurn()}>
+                        End Turn
+                    </Button>
+                </div>
             </BaseContainer>
         );
     }
