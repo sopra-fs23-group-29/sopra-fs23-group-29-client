df04eab81399c7cddbf9fd201973f2f5a6a7a3a9
moonaaaw <116147381+moonaaaw@users.noreply.github.com>
2023-04-18 09:53:38 +0200

    updated leave game

--- src/components/views/HeaderGame.js
+++ src/components/views/HeaderGame.js
@@ -3,69 +3,43 @@ import PropTypes from "prop-types";
 import "styles/views/Header.scss";
 import {Globalissimo} from "../ui/Globalissimo";
 import {api, handleError} from "../../helpers/api";
-import {useHistory} from "react-router-dom";
-import BaseContainer from "../ui/BaseContainer";
+import {useHistory, useParams} from "react-router-dom";
+import Stomper from "../../helpers/Stomp";
 
 const HeaderGame = props => {
     const history = useHistory();
+    const gameId = useParams().id;
+    let webSocket = Stomper.getInstance();
 
     const popUpLeave = async () => {
-        let x = null;
         if (window.confirm("Do you want to leave the game?")) {
-                // leave the game
+            try {
+                // set status to offline
+                await api.delete(
+                    `/games/${gameId}`,
+                    {},
+                    {headers: {"Authorization": JSON.parse(localStorage.getItem('token')).token}}
+                );
 
-                // set dummy gameId
-                let gameId = 1
-
-                try {
-                    // set status to offline
-                    await api.delete(
-                        `/games/${gameId}`,
-                        {},
-                        {headers: {"Authorization": JSON.parse(localStorage.getItem('token')).token}}
-                    );
-
-                } catch (error) {
-                    console.log(`Something went wrong when leaving the game: \n${handleError(error)}`);
-                }
-
-                // remove websocket connection
-                // webSocket.leave()
-                // webSocket.disconnect("User logged out");
-
-                history.push('/');
-
+            } catch (error) {
+                console.log(`Something went wrong when leaving the game: \n${handleError(error)}`);
             }
-    }
 
-    const leaveGame = async () => {
-        // set dummy gameId
-        let gameId = 1
+            // remove websocket connection
+            webSocket.leave(`/games/${gameId}`)
 
-        try {
-            // set status to offline
-            await api.delete(
-                `/games/${gameId}`,
-                {},
-                {headers: {"Authorization": JSON.parse(localStorage.getItem('token')).token}}
-            );
+            // redirect to home
+            history.push('/');
 
-        } catch (error) {
-            console.log(`Something went wrong when leaving the game: \n${handleError(error)}`);
         }
-
-        // remove websocket connection
-        // webSocket.leave()
-        // webSocket.disconnect("User logged out");
-
-        history.push('/');
-    };
+    }
 
     return (
         <div className="header container" style={{height: props.height}}>
             <Globalissimo/>
             <h2 className="header game username">[USERNAME]</h2>
-            <p className="header game barrier-counter">[Barrier Counter]</p>
+            {//<p className="header game barrier-counter">[Barrier Counter]</p>
+            }
             <p className="header game round-counter">[Round Counter]</p>
             <i className="header game icon" onClick={() => popUpLeave()}>logout</i>
         </div>
