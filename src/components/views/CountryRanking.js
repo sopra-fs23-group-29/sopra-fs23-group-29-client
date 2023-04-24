48f25c004191e3bedba19901ccdf15ae76e0b7c2
moonaaaw <116147381+moonaaaw@users.noreply.github.com>
2023-04-24 19:57:09 +0200

    WIP CountryRanking.js

--- src/components/views/CountryRanking.js
+++ src/components/views/CountryRanking.js
@@ -3,169 +3,40 @@ import {useHistory, useParams} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
 import "styles/views/CountryRanking.scss";
 import Stomper from "../../helpers/Stomp";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import Player from "../../models/Player";
 
 const CountryRanking = props => {
-    let test_data =
-        {
-            "rankQuestion": {
-                "questionType": "RANK",
-                "rankQuestionCategory": "POPULATION",
-                "questionText": "Which of these countries has the largest population?",
-                "questionTextShort": "Population",
-                "countryList": [
-                    {
-                        "nameMap": {
-                            "common": "Guyana",
-                            "official": "Co-operative Republic of Guyana",
-                            "nativeName": {
-                                "eng": {
-                                    "official": "Co-operative Republic of Guyana",
-                                    "common": "Guyana"
-                                }
-                            }
-                        },
-                        "area": 214969.0,
-                        "population": 786559,
-                        "giniMap": {
-                            "1998": 45.1
-                        },
-                        "capitalInfoMap": {
-                            "latlng": [
-                                6.8,
-                                -58.15
-                            ]
-                        },
-                        "flagsMap": {
-                            "png": "https://flagcdn.com/w320/gy.png",
-                            "svg": "https://flagcdn.com/gy.svg",
-                            "alt": "The flag of Guyana has a green field with two isosceles triangles which share a common base on the hoist end. The smaller black-edged red triangle spanning half the width of the field is superimposed on the larger white-edged yellow triangle which spans the full width of the field."
-                        },
-                        "cioc": "GUY",
-                        "bordersList": [
-                            "BRA",
-                            "SUR",
-                            "VEN"
-                        ],
-                        "gini": 45.1,
-                        "giniYear": "1998",
-                        "capitalLatitude": 6.8,
-                        "flagUrl": "https://flagcdn.com/gy.svg",
-                        "name": "Guyana"
-                    },
-                    {
-                        "nameMap": {
-                            "common": "Estonia",
-                            "official": "Republic of Estonia",
-                            "nativeName": {
-                                "est": {
-                                    "official": "Eesti Vabariik",
-                                    "common": "Eesti"
-                                }
-                            }
-                        },
-                        "area": 45227.0,
-                        "population": 1331057,
-                        "giniMap": {
-                            "2018": 30.3
-                        },
-                        "capitalInfoMap": {
-                            "latlng": [
-                                59.43,
-                                24.72
-                            ]
-                        },
-                        "flagsMap": {
-                            "png": "https://flagcdn.com/w320/ee.png",
-                            "svg": "https://flagcdn.com/ee.svg",
-                            "alt": "The flag of Estonia is composed of three equal horizontal bands of blue, black and white."
-                        },
-                        "cioc": "EST",
-                        "bordersList": [
-                            "LVA",
-                            "RUS"
-                        ],
-                        "gini": 30.3,
-                        "giniYear": "2018",
-                        "capitalLatitude": 59.43,
-                        "flagUrl": "https://flagcdn.com/ee.svg",
-                        "name": "Estonia"
-                    },
-                    {
-                        "nameMap": {
-                            "common": "Somalia",
-                            "official": "Federal Republic of Somalia",
-                            "nativeName": {
-                                "ara": {
-                                    "official": "جمهورية الصومال‎‎",
-                                    "common": "الصومال‎‎"
-                                },
-                                "som": {
-                                    "official": "Jamhuuriyadda Federaalka Soomaaliya",
-                                    "common": "Soomaaliya"
-                                }
-                            }
-                        },
-                        "area": 637657.0,
-                        "population": 15893219,
-                        "giniMap": {
-                            "2017": 36.8
-                        },
-                        "capitalInfoMap": {
-                            "latlng": [
-                                2.07,
-                                45.33
-                            ]
-                        },
-                        "flagsMap": {
-                            "png": "https://flagcdn.com/w320/so.png",
-                            "svg": "https://flagcdn.com/so.svg",
-                            "alt": "The flag of Somalia features a large five-pointed white star centered on a light blue field."
-                        },
-                        "cioc": "SOM",
-                        "bordersList": [
-                            "DJI",
-                            "ETH",
-                            "KEN"
-                        ],
-                        "gini": 36.8,
-                        "giniYear": "2017",
-                        "capitalLatitude": 2.07,
-                        "flagUrl": "https://flagcdn.com/so.svg",
-                        "name": "Somalia"
-                    }
-                ]
-            }
-        }
     const gameId = useParams().id;
     let webSocket = Stomper.getInstance();
     // dummies
-    let category = test_data.rankQuestion.questionTextShort
     let playerId = 3
     let countryCode = "Switzerland"
 
-    const [game, setGame] = useState(null)
-    const [countries, setCountries] = useState([]);
-    const [flags, setFlags] = useState([]);
-    const [flagAlt, setFlagAlt] = useState([]);
+    // websocket variables
     const [turnNumber, setTurnNumber] = useState(null);
     const [turnPlayers, setTurnPlayers] = useState(null);
     const [rankQuestion, setRankQuestion] = useState(null);
-    const [guess, setGuess] = useState(2);
+    const [takenGuesses, setTakenGuesses] = useState(null);
 
-    // set country names
-    for (let i = 0; i < test_data.rankQuestion.countryList.length; i++) {
-        countries.push(test_data.rankQuestion.countryList[i].nameMap.common)
-        flags.push(test_data.rankQuestion.countryList[i].flagsMap.svg)
-        flagAlt.push(test_data.rankQuestion.countryList[i].flagsMap.alt)
-    }
+    // countries and flags
+    const [countries, setCountries] = useState([]);
+    const [flags, setFlags] = useState([]);
+    const [flagAlt, setFlagAlt] = useState([]);
 
-    const [gameInfo, setGameInfo] = useState(null);
+    // Player
+    const [playerList, setPlayerList] = useState([]);
+
+    // misc
+    const [category, setCategory] = useState([]);
+    const [checkedMarker, setCheckedMarker] = useState(null);
+    const [checkedCountry, setCheckedCountry] = useState(null);
+
 
     useEffect(() => {
         async function fetchData() {
             /* subscribe to topic/games/{gameId} */
-            webSocket.join("/topic/games/" + gameId, processResponse);
+            //webSocket.join("/topic/games/" + gameId + "/newturn", processResponse);
 
             /* Get the current game */
             //webSocket.send("/app/games/" + gameId + "/nextTurn", { message: "NEXT TURN" });
@@ -177,18 +48,38 @@ const CountryRanking = props => {
             webSocket.send("/app/games/" + gameId + "/nextTurn", { message: "NEXT TURN" });
         }
 
-    }, [gameInfo]);
+    }, []);
 
     const processResponse = message => {
-        setGameInfo(JSON.parse(message.body));
+        // set turnNumber
+        setTurnNumber(JSON.parse(message.body).turnNumber);
+
+        // set turnPlayers
+        setTurnPlayers(JSON.parse(message.body).turnPlayers);
+
+        // set rankQuestion
+        setRankQuestion(JSON.parse(message.body).rankQuestion);
+
+        // set takenGuesses
+        setTakenGuesses(JSON.parse(message.body).takenGuesses);
+
+        // set players
+        for (let i = 0; i < JSON.parse(message.body).turnPlayers.length; i++) {
+            let player = new Player(JSON.parse(message.body).turnPlayers[i])
+            playerList.push(player)
+        }
+
+        // set country names
+        for (let i = 0; i < JSON.parse(message.body).rankQuestion.countryList.length; i++) {
+            countries.push(JSON.parse(message.body).rankQuestion.countryList[i].nameMap.common)
+            flags.push(JSON.parse(message.body).rankQuestion.countryList[i].flagsMap.svg)
+            flagAlt.push(JSON.parse(message.body).rankQuestion.countryList[i].flagsMap.alt)
+        }
+
+        // set category
+        setCategory(JSON.parse(message.body).rankQuestion.questionTextShort)
     };
 
-
-    const getCountries = () => {
-        webSocket.addEventListener("message", ({ data }) => {
-            const event = JSON.parse(data);
-        });
-    }
 
     // End Turn
     const saveAnswer = () => {
@@ -196,11 +87,31 @@ const CountryRanking = props => {
         webSocket.send(
             `/app/games/${gameId}/turn/${turnNumber}/player/${playerId}/saveAnswer`,
             {
-                userToken: JSON.parse(localStorage.getItem("token")).token,
+                userToken: JSON.parse(sessionStorage.getItem("token")).token,
                 countryCode: countryCode,
-                guess: guess,
+                takenGuesses: takenGuesses,
             }
             */
+        // check which buttons are checked
+        /*
+        for (let i = 0; i < countries.length; i++) {
+            let currentMarker = document.getElementById('marker' + [i]);
+            let currentCountry = document.getElementById('country' + [i]);
+            // checking if any radio button is selected
+            if(currentMarker.checked){
+                console.log("The radio button with value " + currentMarker.key + " is checked!");
+            }
+            if(currentCountry.checked){
+                console.log("The radio button with value " + currentCountry.key + " is checked!");
+            }
+        }
+         */
+        console.log("turnNumber" + turnNumber)
+        console.log("turnPlayers" + turnPlayers)
+        console.log("rankQuestion" + rankQuestion)
+        console.log("text" + rankQuestion.countryList[0].nameMap.common)
+        console.log("takenGuesses" + takenGuesses)
+        //console.log(document.querySelectorAll('input[name=marker]:checked'))
         webSocket.send("/app/games/" + gameId + "/nextTurn", { message: "NEXT TURN" });
     };
 
@@ -217,7 +128,7 @@ const CountryRanking = props => {
                 <div>
                     <div className="country-ranking marker-container">
                         <label className="country-ranking marker-number" onClick={() => setMarkerChecked(`marker${i}`)}>{i}</label>
-                        <input type="radio" name="marker" id={"marker" + i } className="marker-radio" value={"player" + playerId}/>
+                        <input type="radio" name="marker" id={"marker" + i} className="marker-radio" value={"player" + playerId}/>
                     </div>
                 </div>
             )
@@ -280,7 +191,9 @@ const CountryRanking = props => {
                 <div className="country-ranking number-row">
                     {marker(countries)}
                 </div>
-                <Button onClick={() => saveAnswer()}>
+                <Button
+                    id="saveAnswer"
+                    onClick={() => saveAnswer()}>
                     End Turn
                 </Button>
             </div>
