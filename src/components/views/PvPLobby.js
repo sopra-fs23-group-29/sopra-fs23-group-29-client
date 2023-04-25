c321ca9adf73ce12491870e7d89d986ac1313ca5
Darnol <32634736+Darnol@users.noreply.github.com>
2023-04-25 17:00:56 +0200

    Add /gamedeleted, leave lobby now works for player and host

--- src/components/views/PvPLobby.js
+++ src/components/views/PvPLobby.js
@@ -5,6 +5,7 @@ import "styles/views/Login.scss";
 import "styles/views/Home.scss";
 import BaseContainer from "components/ui/BaseContainer";
 import Stomper from "../../helpers/Stomp";
+import { api, handleError } from "helpers/api";
 
 /* This is the view for an open PvP Game Lobby that displays its name and all players in the lobby  */
 
@@ -34,6 +35,9 @@ const PvPLobby = (props) => {
 
       /* subscribe to topic/games/{gameId}/gamestart to get an update if the game starts */
       webSocket.join(`/topic/games/${gameId}/gamestart`, gameHasStarted);
+
+      /* subscribe to topic/games/{gameId}/gamedeleted to get an update if the game is deleted due to host leaving */
+      webSocket.join(`/topic/games/${gameId}/gamedeleted`, gameHasEnded);
 
       /* Get the current game */
       webSocket.send("/app/games/" + gameId + "/getGame", {
@@ -68,6 +72,14 @@ const PvPLobby = (props) => {
     history.push(`/games/` + id);
   };
 
+  const gameHasEnded = (message) => {
+    const id = params.id;
+    webSocket.leave(`/topic/games/${id}/lobby`);
+    webSocket.leave(`/topic/games/${id}/gamestart`);
+    webSocket.leave(`/topic/games/${id}/gamedeleted`);
+    history.push("/");
+  };
+
   /* starts the game with all the players that are currently in the lobby*/
   const startGame = () => {
     const id = params.id;
@@ -82,14 +94,32 @@ const PvPLobby = (props) => {
     console.log("Game with ID " + id + " has been started");
   };
 
-  const exitLobby = () => {
-    /* check if person who wants to exit is the creator of the lobby*/
+  async function exitLobby() {
 
-    /* if person exiting is creator of lobby: delete lobby (and game on server) and 
-        redirect all players to the home screen*/
-
-    /* if person exiting is just a player: delete them as player and redirect to home screen,
-        update player list for all remaining players */
+      const id = params.id;
+  
+      try {
+        // fetch the user trying to leave the game
+        const token = JSON.parse(sessionStorage.getItem("token")).token;
+        console.log("leave game: token " + token);
+        // If the user is not authorized, this REST request will fail
+        const response = await api.delete(`/games/` + id, {
+          headers: { Authorization: token },
+        });
+  
+        // if the player leaving was the host and the game was in lobby, the backend will send a message to /games/id/gamedeleted
+  
+        /* unsubscribe from all lobby channels */
+        webSocket.leave(`/topic/games/${id}/lobby`);
+        webSocket.leave(`/topic/games/${id}/gamestart`);
+        webSocket.leave(`/topic/games/${id}/gamedeleted`);
+  
+        // Edit successfully worked --> navigate to the route /profile/id
+        console.log("Left game");
+  
+      } catch (error) {
+        alert(`Something went wrong while leaving game: \n${handleError(error)}`);
+      }
 
     history.push(`/`);
   };
