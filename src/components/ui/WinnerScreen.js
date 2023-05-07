311c09a4030e90ad59606e87fa8ba8747984795c
Nilsils <nils.bohnenblust@gmail.com>
2023-05-07 19:48:02 +0200

    fixed imports of winnerscreen

--- src/components/ui/WinnerScreen.js
+++ src/components/ui/WinnerScreen.js
@@ -2,45 +2,23 @@ import BaseContainer from "./BaseContainer";
 import "styles/views/WinnerScreen.scss";
 import { Button } from "./Button";
 import React from "react";
-import { Barrier } from "./Barrier";
+import { useParams, useHistory } from "react-router-dom";
+import Stomper from "../../helpers/Stomp";
+import { useEffect, useState } from "react";
+import { api, handleError } from "../../helpers/api";
 
 export const WinnerScreen = (props) => {
 
-  let test_data = {
-    "winnerList": [
-      {
-        "rank": 1,
-        "username": "Nils1dfsdfs",
-        "playercolor": "green",
-        "answeredBarrierQuestions": 5
-      },
 
-      {
-        "rank": 2,
-        "username": "Nils2",
-        "playercolor": "red",
-        "answeredBarrierQuestions": 3
-      },
-
-      {
-        "rank": 3,
-        "username": "Nils3",
-        "playercolor": "yellow",
-        "answeredBarrierQuestions": 1
-      },
-
-      {
-        "rank": 4,
-        "username": "Nils4",
-        "playercolor": "blue",
-        "answeredBarrierQuestions": 1
-      }
-    ]
-  }
+  const history = useHistory();
+  const params = useParams();
+  let gameId = params.id;
 
   //const [players, setPlayers] = useState(props.players);
   const [globalScoreboard, setGlobalScoreboard] = useState(props.leaderboard);
   const [barrierScoreboard, setBarrierScoreboard] = useState(props.barrierScoreboard);
+
+  let webSocket = Stomper.getInstance();
 
   useEffect(() => {
     setGlobalScoreboard(props.leaderboard);
