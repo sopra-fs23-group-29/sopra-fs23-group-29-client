66c9e1d0c69b72a1ac8249c10e7a6fa44532d600
Darnol <32634736+Darnol@users.noreply.github.com>
2023-05-17 18:45:29 +0200

    Restrict Registration to letter, digits and underscore

--- src/components/views/Registration.js
+++ src/components/views/Registration.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { api, handleError } from "helpers/api";
 import User from "models/User";
 import { useHistory } from "react-router-dom";
@@ -39,6 +39,8 @@ const Registration = (props) => {
   const history = useHistory();
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
+  const [usernameChecked, setUsernameChecked] = useState(false);
+  const [passwordChecked, setPasswordChecked] = useState(false);
 
   const goToLogin = () => {
     history.push("/login");
@@ -72,6 +74,32 @@ const Registration = (props) => {
     }
   };
 
+  useEffect(() => {
+    setUsernameChecked(checkString(username));
+  }, [username])
+
+  useEffect(() => {
+    setPasswordChecked(checkString(password));
+  }, [password])
+  
+  const checkString = (stringToCheck) => {
+
+    if (!stringToCheck) {
+      return false;
+    }
+
+    // check if only digits and letters
+
+    // if only 1 character, must be A-Za-z0-9
+    if (stringToCheck.length === 1) {
+      return /[A-Za-z0-9]/.test(stringToCheck)
+    }
+
+    // _ is allowed in the middle
+    return /^[A-Za-z0-9]{1}[_A-Za-z0-9]*[A-Za-z0-9]{1}$/.test(stringToCheck);
+
+  }
+
   return (
     <BaseContainer>
       <div className="login container">
@@ -88,7 +116,7 @@ const Registration = (props) => {
           />
           <div className="login button-container">
             <Button
-              disabled={!username || !password}
+              disabled={!username || !password || !usernameChecked || !passwordChecked}
               width="100%"
               onClick={() => doRegistration()}
             >
@@ -97,6 +125,9 @@ const Registration = (props) => {
             <Button width="100%" onClick={() => goToLogin()}>
               To login
             </Button>
+          </div>
+          <div className="login label">
+            Only use letters and digits and underscores. No leading or trailing underscores.
           </div>
         </div>
       </div>
